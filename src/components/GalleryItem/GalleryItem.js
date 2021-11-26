b4a5ea70ad9e9a028b08003a3209c33d3443aa95
Bennett Smrdel <smrdelb@gmail.com>
2021-11-26 11:12:02 -0600

    started adding conditional rendering functions

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 
 function GalleryItem({galleryData}) {
+    const [showImage, setShowImage] = useState(true);
+
+    const flipImage = () => {
+        if (showImage) {
+          setShowImage(false);
+        } else {
+          setShowImage(true);
+        }
+    };
+
     return (
         <>
             {/* Loop through the gallery list array and render the images, and text. */}
             {galleryData.map((galleryData) => {
-                return <img key={galleryData.id} src="images/goat_small.jpg"/>
+                return (
+                    <img key={galleryData.id} 
+                        src={galleryData.path}
+                        onClick={flipImage}/>
+                );
             })}
         </>
     );
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -3,7 +3,6 @@ import GalleryItem from "../GalleryItem/GalleryItem";
 function GalleryList({galleryData}) {
     return (
         <div>
-            <p>Gallery goes here</p>
             <GalleryItem galleryData={galleryData} />
         </div>
     );
