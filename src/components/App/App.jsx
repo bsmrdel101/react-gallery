5f7046f12d47ba9114d760483c9834cb57c1d16a
Bennett Smrdel <smrdelb@gmail.com>
2021-11-26 08:27:53 -0600

    set up project, moved header and gallery into different components, in order to clear up space in App.js.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+
+// Import components
+import Header from '../Header/Header';
+import Gallery from '../Gallery/Gallery';
 
 function App() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
+        <Header />
+        <Gallery />
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
       </div>
